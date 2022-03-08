ef5fb99c9fbf479b1e0688176a076acb01d33c20
Moharram Moharrami <moharram.m98@gmail.com>
2022-03-08 17:46:52 +0330

    fix some typos in js

--- code.js
+++ code.js
@@ -1,15 +1,15 @@
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
-        const postsArr = data.slice(0, 5);
-        let html = "";
+        const postsArr = data.slice(0, 5)
+        let html = ""
         for (let post of postsArr) {
             html += `
                 <h3>${post.title}</h3>
                 <p>${post.body}</p>
                 <hr />
             `
-        };
-
-        document.getElementById("blog-post").innerHTML = html;
+        }
+        
+        document.getElementById("blog-list").innerHTML = html
     })
