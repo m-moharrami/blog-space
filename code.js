4e2aa631b68941b00e121a50e35bad7c523e96f0
Moharram Moharrami <moharram.m98@gmail.com>
2022-03-09 18:54:16 +0330

    send posts to the server

--- code.js
+++ code.js
@@ -22,9 +22,20 @@ document.getElementById("new-post").addEventListener('submit', function(event) {
     event.preventDefault();
     const postTitle = document.getElementById("post-title").value;
     const postBody = document.getElementById("post-body").value;
-    const article = {
+    const data = {
         title: postTitle,
         body: postBody
     };
-    console.log(article);
+    
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+    
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(data => console.log(data))
 })
