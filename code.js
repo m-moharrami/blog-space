7a27fe6db294bd299def252619422cd402d88525
Moharram Moharrami <moharram.m98@gmail.com>
2022-03-09 16:24:00 +0330

    add event listener to form submit

--- code.js
+++ code.js
@@ -10,6 +10,21 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
                 <hr />
             `
         }
-        
+
         document.getElementById("blog-list").innerHTML = html
     })
+
+const article = new Object();
+article.body = '';
+article.title = '';
+
+document.getElementById("new-post").addEventListener('submit', function(event) {
+    event.preventDefault();
+    const postTitle = document.getElementById("post-title").value;
+    const postBody = document.getElementById("post-body").value;
+    const article = {
+        title: postTitle,
+        body: postBody
+    };
+    console.log(article);
+})
